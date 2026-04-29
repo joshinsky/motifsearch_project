1762bea310bda5f4720c29b4eebbc1cb5ae40843
joshinsky <joshinsky@outlook.de>
2026-04-29 16:14:57 +0200

    add recursive logic to motifind.py so that FINALLY the algorithm works - hallelujah

--- Motifind.py
+++ Motifind.py
@@ -13,9 +13,9 @@ def usage(msg=None):
     sys.exit(1)
 
 
-######
-# FastaRead
-######
+###############
+## FastaRead ##
+###############
 
 # Reads a fasta file and returns two lists: headers and sequences
 def FastaRead(filename):
@@ -38,12 +38,12 @@ def FastaRead(filename):
 
                 # Check for header line starting with ">"
                 if line.startswith('>'):
-                    # Save the in progress sequence, if any
+                    # Save the in progress sequence, if any, then save the header
                     if current_seq:
                         sequences.append("".join(current_seq))
                         current_seq = []
+                    headers.append(line)
 
-                    headers.append(line[1:])   # Store the header without the ">"
                 else:
                     # Append sequence line to fragments list
                     current_seq.append(line)
@@ -63,9 +63,10 @@ def FastaRead(filename):
         raise IOError(f"Error reading FASTA file: {e}")
 
 
-######
-# MotifRead
-######
+
+###############
+## MotifRead ##
+###############
 
 def MotifRead(filename):
 
@@ -74,7 +75,6 @@ def MotifRead(filename):
         raise FileNotFoundError(f"The file {filename} does not exist.")
 
     motif = []
-    penalties = []
 
     try:
         with open(filename, 'r') as f:
@@ -85,99 +85,207 @@ def MotifRead(filename):
                 if not line or line.startswith('#'): 
                     continue
 
-
                 parts = line.split('\t')
                 if len(parts) < 2:
-                    raise IndexError(f"Missing penalty column:\nexpected:\t[letter, penalty]\ngiven:\t{line}")
+                    raise IndexError(f"Missing second column:\nexpected:\t[letter, penalty]\ngiven:\t{line}")
 
-                motif_letter = parts[0]
-                penalty = parts[1]
+                if parts[0] == '*':
+                    entry_type = 'gap'
+                    gap_bounds = parts[1].split('-')
+                    try:
+                        lower_bound = int(gap_bounds[0])
+                        upper_bound = int(gap_bounds[1])
+                    except ValueError:
+                        raise ValueError(f"Error: gap bounds should be given as integers.\n{gap_bounds} was given which are not integers.")    
+                    motif_entry = (entry_type, lower_bound, upper_bound)
+                    
+                else:
+                    entry_type = 'char'
+                    motif_chars = set(parts[0])
+                    penalty = parts[1]
+                    try:
+                        penalty = float(penalty)
+                    except ValueError:
+                        raise ValueError(f"Error: penalties should be given as numbers.\nYour input '{penalty}' is non-numerical.")
+                    motif_entry = (entry_type, motif_chars, penalty)
                 
-                motif.append(motif_letter)
-                penalties.append(penalty)
+                motif.append(motif_entry)
 
     # Error handling for unexpected issues
     except IOError as e:
         raise IOError(f"Error reading motif file: {e}")
 
-    # Check for the equality of headers and sequences amount
-    if len(motif) != len(penalties):
-        raise ValueError("Mismatch between the length of motif and penalties.")
+    return motif
 
-    return motif, penalties
 
-######
-# MotifTracker
-######
 
-def MotifTracker(sequences, headers, motif, penalties, max_penalty):
-    # List to store motif matches
-    matches = []
+#################
+## MatchFinder ##
+#################
+
+def MatchFinder(seq, current_seq_idx, motif, current_motif_idx, accum_penalty, max_penalty):
+
+    if current_seq_idx > len(seq):
+        matchlist = []
+        return matchlist
+
+    if current_motif_idx >= len(motif):
+        matchlist = [[current_seq_idx, accum_penalty]]
+        return matchlist
+
+    if current_seq_idx == len(seq):
+        matchlist = []
+        return matchlist
+
+    if motif[current_motif_idx][0] == 'char':
+        seq_char = seq[current_seq_idx]
+        motif_chars = motif[current_motif_idx][1]
+        mismatch_penalty = motif[current_motif_idx][2]
+
+        if seq_char not in motif_chars:
+            accum_penalty += mismatch_penalty
+            if accum_penalty > max_penalty:
+                matchlist = []
+                return matchlist
+
+        current_seq_idx += 1
+        current_motif_idx += 1  
+        return MatchFinder(seq, current_seq_idx, motif, current_motif_idx, accum_penalty, max_penalty)
+
+    elif motif[current_motif_idx][0] == 'gap':
+        min_gap_len = motif[current_motif_idx][1]
+        max_gap_len = motif[current_motif_idx][2] + 1
+        
+        current_motif_idx += 1
+        gap_matches = []
+        for i in range(min_gap_len, max_gap_len):
+            gap_accum_penalty = accum_penalty
+            gap_seq_idx = current_seq_idx+i
+            matchlist = MatchFinder(seq, gap_seq_idx, motif, current_motif_idx, gap_accum_penalty, max_penalty)
+            gap_matches.extend(matchlist[:])
+        return gap_matches
+
+
+##################
+## Main Program ##
+##################
+
+if __name__ == "__main__":
+
+    # get user inputs
+    try:
+        fasta_name = sys.argv[1]
+        motif_name = sys.argv[2]
+        max_penalty = int(sys.argv[3])
+    except IndexError:
+        usage(msg="Not enough input arguments given.")
+    except ValueError:
+        usage(msg="please give deviation as integer.")
+
+    # extract fasta
+    headers, sequences = FastaRead(fasta_name)
+    print(f"{len(headers)} fasta entries successfully extracted from {fasta_name}")
     
-    # Length of the motif we are searching for
-    motif_len = len(motif)
+    # extract motif
+    motif = MotifRead(motif_name)
+    print(f"The motif provided in {motif_name} has a minimum length of {len(motif)}.")
+    print(f"So, let's get started with a maximum mismatch score of {max_penalty}!")
 
-    # Loop through each sequence in the fasta file
-    for seq_idx, seq in enumerate(sequences):
+    # find matches
+    all_matches = {}
+    for i in range(len(sequences)):
+        seq = sequences[i]
+        head = headers[i]
+        all_matches[head] = []
+        
+        start_idx = 0
+        for start_idx in range(len(seq)):
+            motif_idx = 0
+            start_penalty = 0
+            matches = MatchFinder(seq, start_idx, motif, motif_idx, start_penalty, max_penalty)
+        
+            for match in matches:
+                stop_idx = match[0]
+                final_penalty = match[1]
+                all_matches[head].append((start_idx, stop_idx, final_penalty))
 
-        # Slide a window of motif length across the sequence
-        for start in range(len(seq) - motif_len + 1):
-            # Reset penalty counter for this starting position
-            tot_penalty = 0
+    # finish up
+    user_prompt1 = f"""
+    \nProgram finished!
+    \nFound one or more matches for {len(all_matches)} entries!
+    \nHow would you like to proceed?
+    """ 
+    
+    user_prompt2 = """
+    \nPlease select by typing one of the below numbers in the interface:
+    \n1 --> save matches to disk
+    \n2 --> output matches without saving
+    \n3 --> output matches and save to disk
+    \n4 --> stop program without saving matches
+    """
+    allowed_answers = {'1', '2', '3', '4', '42'}
+    user_answer = '0'
 
-            mismatch = False
+    print(user_prompt1)
+    while user_answer not in allowed_answers:
+        user_answer = input(user_prompt2)
 
-            # Compare motif to sequence character by character
-            for i in range(motif_len):
-                # Character from sequence at current position
-                seq_char = seq[start + i]
+        if user_answer in {'1', '3'}:
+            output_filename = input("Type the desired name for the file here (without file-suffix):")
+            with open(f"{output_filename}.tsv", 'w') as outfile:
+                for head, matchlist in all_matches.items():
+                    print(head, file=outfile)
+                    for match in matchlist:
+                        print(match, file=outfile)
+        
+        if user_answer in {'2', '3'}:
+            for head, matchlist in all_matches.items():
+                print(head)
+                for match in matchlist:
+                    print(match)
 
-                # Expected motif character at this position
-                motif_char = motif[i]
+        if user_answer == '4':
+            print("\nWell, okay then...\nThanks for wasting both your and my time, I guess...")
 
-                # If characters do not match add penalty
-                if seq_char != motif_char:
-                    tot_penalty += penalties[i]
+        if user_answer == '42':
+            print("""
+                \nCongrats! You found the hidden easter egg in this program!
+                \nGood job!
+                \nNow that you've made it here, you don't need to know all matches in 
+                \nyour fasta files anymore! I will not print them for you.
+                \nYou know the answer to life, the universe and all that lies beyond 
+                \nanyway, otherwise you wouldn't have chosen this number.
+                \nSo, why are you still here bothering yourself with these problems?
+                \nFASTA files... sequence matches... recursive algorithms...
+                \nIt all becomes so small if you sometimes just take a step back and
+                \nreflect on life and its beauty.
+                \nDo you really want to sit in this dark closed room with little to no
+                \noxygen in front of a screen that will be bad for your eyesight and 
+                \nyour posture? Chances are that the weather is quite nice outside.
+                \nIf you think about it carefully, outside is where you really want
+                \nto be right now. Isn't it?
+                \nI know we all have a mission - a cause that we dedicate ourselves to.
+                \nFind motifs, find mutations, cure diseases, save the world...
+                \nBut who is there to save you today from the dullness of the day?
+                \nThe answer is... 42 (of course), but that only means that it's in
+                \nyour own power to take a step back, carefully reach out with your
+                \nhand of preference, either close the laptop or switch off the PC, 
+                \ntake another step back, try out a smile, feel these rarely-used 
+                \nmuscles stretch your face, look towards the door, and materialise
+                \nthe thought of fresh air in your lungs, the feeling of warm sunshine
+                \non your skin, and the happiness and relaxedness flooding your whole
+                \nbody slowly. 
+                \nIf this thought develops its own power, don't resist, but give in.
+                \nStep towards the door, open it and step outside, leave the building,
+                \ngo for a walk, hear the birds sing.
+                \nTake a break. You deserve it. Give it to yourself as a present.
+                \n
+                \nBut in case it's a rainy day, you can feel free to continue working.
+                """)
+            sys.exit(0)
 
-                    # Stop checking this position if penalty exceeds limit
-                    if tot_penalty > max_penalty:
-                        mismatch = True
-                        break
-  
-            # If we did not exceed penalty threshold, record the match
-            if not mismatch:
-                matches.append(
-                    (
-                        headers[seq_idx],       # Sequence identifier
-                        start,                  # Start position of the match
-                        start + motif_len - 1,  # End position of match
-                        tot_penalty             # Total penalty score
-                    )
-            )
 
-    return matches
 
-######
-# Run Program
-######
 
-# get inputs
-try:
-    fasta_name = sys.argv[1]
-    motif_name = sys.argv[2]
-    maxpenalty = int(sys.argv[3])
-except IndexError:
-    usage(msg="Not enough input arguments given.")
-except ValueError:
-    usage(msg="please give deviation as integer.")
 
-# extract fasta
-headers, sequences = FastaRead(fasta_name)
-print(headers)
-print(sequences)
-
-# extract motif
-motif, penalties = MotifRead(motif_name)
-# print(motif)
-# print(penalties)
 
